f946e11e3db581030aff45554aa7a0651b5b7df2
wawan-ikhwan <ikhwanperwira@gmail.com>
2021-12-04 21:31:22 +0700

    project PP

---
+++ src/client/client.py
@@ -0,0 +1,105 @@
+from socket import timeout, socket
+from threading import Thread
+import time
+import os
+import glob
+import requests
+from pytrends.request import TrendReq
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+os.system('mkdir WLpart')
+os.system('mkdir CSVpart')
+os.system('mkdir AVGpart')
+
+interupsi=False
+IP,PORT=('27.112.79.120',12345)
+#IP,PORT=('127.0.0.1',12345)
+
+s=socket()
+s.connect((IP,PORT))
+
+# ======================THREAD CLIENT==================================
+sessID=None
+def clientReceive():
+  global interupsi
+  global sessID
+  while not interupsi:
+    time.sleep(0.1)
+    try:
+      s.settimeout(1)
+      if not interupsi:
+        buffer=s.recv(1024).decode()
+        print(buffer)
+        if sessID is None and 'sessID=' in buffer:
+          #deleteAllFilesInFolder('./WLpart')
+          sessID=buffer[buffer.rfind('=')+1:] #filter char '=' karena didapat dari pesan server ketika client pertama kali masuk
+          open('sessID.txt','w').write(sessID)
+          # Dapatkan file pembagian tugas...
+          getData()
+    except:
+      pass
+  exit()
+
+def clientSend():
+  global interupsi
+  global sessID
+  while not interupsi:
+    time.sleep(0.1)
+    pesan=input()
+    print ('\033[A                                              \033[A') # clear last line
+    if pesan == '!keluar':
+      interupsi=True
+    elif pesan == '!put':
+      putData(sessID+'.avg')
+    elif pesan == '!get':
+      getData()
+    elif pesan == '!fetch':
+      fetching(sessID)
+    s.send(pesan.encode())
+  exit()
+# =============================================================
+
+def fetching(sessID,sleepTime=5):
+  wl=open('./WLpart/'+sessID+'.txt').read().splitlines()
+  banyakBaris=len(wl)-1
+  trend=TrendReq(hl='id',timeout=(10,25),retries=2, backoff_factor=0.1, requests_args={'verify':False})
+  for i in range(banyakBaris):
+    target=wl[i]
+    kw_list = [target]
+    trend.build_payload(kw_list, cat=0, timeframe='today 5-y', geo='ID', gprop='')
+    df=trend.interest_over_time()
+    df.to_csv('./CSVpart/'+target+'.csv')
+    avg=target+','+str(int(df.mean(numeric_only=True)))
+    print(avg+str(' | ')+str(i)+'/'+str(banyakBaris)+'\n')
+    open('./AVGpart/'+sessID+'.avg','a+').write(avg)
+    time.sleep(sleepTime)
+
+def putData(fileName):
+  headers = {'Content-Type': 'text/plain'}
+  url='http://27.112.79.120:8000/'+fileName
+  r = requests.put(url, data=open('./AVGpart/'+fileName, 'rb'),headers=headers)
+
+def getData():
+  url = 'http://27.112.79.120:8000/splitted/'+sessID+'.txt'
+  r = requests.get(url)
+  open('./WLpart/'+sessID+'.txt', 'wb').write(r.content)
+
+def deleteAllFilesInFolder(path): # PERINGATAN: GUNAKAN SECARA HATI-HATI!
+  files = glob.glob(path+'/*')
+  for f in files:
+      os.remove(f)
+
+if __name__=='__main__':
+  threadClientReceive=Thread(target=clientReceive,args=())
+  threadClientSend=Thread(target=clientSend,args=())
+  threadClientReceive.start()
+  threadClientSend.start()
+  try:
+    while True and not interupsi: time.sleep(1) # Menunggu interupsi
+  except KeyboardInterrupt:
+    interupsi=True
+  print('Anda keluar!')
+  threadClientReceive.join()
+  threadClientSend.join()
+  s.close()
---
+++ src/server/server.py
@@ -0,0 +1,139 @@
+from socket import AF_INET, SOCK_STREAM, SO_REUSEADDR,SOL_SOCKET, timeout, socket
+from threading import Thread
+from time import sleep
+import pandas as pd
+import os
+import glob
+
+IP,PORT=('0.0.0.0',12345) # Environtment Config, edit terserah user
+
+clients=[] #clients[posisisKlien][1=(ip,port)][0=ip,1=ports]
+errors=(ConnectionAbortedError, ConnectionError, ConnectionRefusedError, ConnectionResetError, TimeoutError, timeout)
+
+s = socket(AF_INET, SOCK_STREAM)
+s.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1) # Mencegah OSError
+s.bind((IP,PORT))
+s.listen()
+
+# ========================================THREAD SERVER===============================
+def sebarkanPesan(pesan,user='SERVER'): # Fungsi dimana server akan menyebarkan pesan ke semua client
+  msg=user.ljust(15)+': '+pesan
+  print('\r'+msg)
+  for c in clients:
+    try:
+      c[0].settimeout(1)
+      c[0].send(('\r'+msg).encode())
+    except errors:
+      clients.remove(c)
+
+def serverInput(): # Fungsi dimana server dapat broadcast pesan
+  global interupsi
+  while not interupsi:
+    sleep(0.1)
+    serverInp=input()
+    print ('\033[A                                                  \033[A') # clear last line
+    if serverInp == '!cek':
+      sebarkanPesan('Mengecek client yang online!')
+      printClients('Online Clients',clients)
+    elif serverInp == '!keluar':
+      interupsi=True
+      print('keluar')
+    elif serverInp == '!splitWL':
+      splitWL(len(clients)) # splitWL berdasarkan banyak clients
+      #splitWL(3)
+      sebarkanPesan('Server membelah wordlist!')
+    elif serverInp == '!combineHasil':
+      combineHasil('./wordlist/destination')
+      sortHasil('./wordlist/destination')
+      sebarkanPesan('Server menggabungkan dan menyortir hasil!')
+    else:
+      sebarkanPesan(serverInp)
+
+def serverAccept(): # Fungsi dimana server dapat menerima sambungan koneksi
+  global interupsi
+  while not interupsi:
+    sleep(0.1)
+    try:
+      s.settimeout(1)
+      clients.append(s.accept())
+      sebarkanPesan(str(clients[-1][1]) + ' telah terhubung!\n'+'sessID='+str(len(clients)))
+    except errors:
+      pass
+
+def serverReceive(): # Fungsi dimana server dapat menerima pesan
+  global interupsi
+  global clients
+  while not interupsi:
+    sleep(0.1)
+    for c in clients:
+      try:
+        try:
+          c[0].settimeout(1)
+          pesan=c[0].recv(1024).decode()
+          if len(pesan) > 0: # Jika pesan ada isi
+            if pesan == '!put':
+                sebarkanPesan(pesan,user=c[1][0])
+            else:
+              sebarkanPesan(pesan,user=c[1][0])
+          else:
+            clients.remove(c)
+        except timeout:
+          pass
+      except errors:
+        clients.remove(c)
+# ========================================================================================
+
+def printClients(judul,data):
+  print()
+  print(judul.center(50,'='))
+  for i,d in enumerate(data):
+    print(str(i)+'.',str(d[1]).ljust(46),end='|\n')
+  print('='*50)
+
+def splitWL(belah):
+  wordlist=open('./wordlist/source/kota-kabupaten.txt','r').read().splitlines()
+  banyakBaris=len(wordlist)
+  jarakBaris=range(0,banyakBaris,banyakBaris//belah)
+  print('Jarak baris:',list(jarakBaris))
+  deleteAllFilesInFolder('./wordlist/splitted')
+  for pointer in range(len(jarakBaris)-1):
+    # namaFile=str(pointer)+'_'+str(jarakBaris[pointer])+'-'+str(jarakBaris[pointer+1])+'.txt'
+    namaFile=str(pointer+1)+'.txt'
+    buffLines=wordlist[jarakBaris[pointer]:jarakBaris[pointer+1]]
+    open('./wordlist/splitted/'+namaFile,'w').write('\n'.join(buffLines))
+
+def combineHasil(path):
+  open(path+'/hasil.csv','w').write('kota,trend\n')
+  files = glob.glob(path+'/*')
+  for f in files:
+    if '.avg' in f:
+      open(path+'/hasil.csv','a').write(open(f,'r').read())
+
+def sortHasil(path):
+  df=pd.read_csv(path+'/hasil.csv')
+  df.sort_values(by=["trend"],ascending=False).to_csv(path+'/sortedHasil.csv',index=False)
+
+def deleteAllFilesInFolder(path): # PERINGATAN: GUNAKAN SECARA HATI-HATI!
+  files = glob.glob(path+'/*')
+  for f in files:
+      os.remove(f)
+
+if __name__=='__main__':
+  print('\rServer berjalan pada',IP,':',PORT,'\n')
+  interupsi=False
+  threadServerInput=Thread(target=serverInput,args=()) # digunakan untuk mengirim input ke semua clients
+  threadServerAccept=Thread(target=serverAccept,args=())
+  threadServerReceive=Thread(target=serverReceive,args=())
+  threadServerInput.start()
+  threadServerAccept.start()
+  threadServerReceive.start()
+  try:
+    while True and not interupsi: sleep(1) # Menunggu interupsi
+  except KeyboardInterrupt:
+    interupsi=True
+  threadServerInput.join()
+  threadServerAccept.join()
+  threadServerReceive.join()
+  s.close()
+  print('Server dimatikan!')
+
